541b78d5e4d4bee3de91485d83a9dfdb6957a85d
swr0408 <swr0408@yahoo.co.jp>
2024-06-10 14:35:43 +0900

    Add files via upload

--- src/main/java/com/example/sample1app/Sample1appApplication.java
+++ src/main/java/com/example/sample1app/Sample1appApplication.java
@@ -1,9 +1,9 @@
 package com.example.sample1app;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
