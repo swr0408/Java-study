da1d565a6d1023823bf5a4dd0909e8dd34c24bc7
swr0408 <swr0408@yahoo.co.jp>
2024-06-10 14:07:13 +0900

    Add files via upload

--- src/main/java/com/example/sample1app/Sample1appApplication.java
+++ src/main/java/com/example/sample1app/Sample1appApplication.java
@@ -1,12 +1,25 @@
 package com.example.sample1app;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Sample1appApplication {
 
+    @Value("${spring.datasource.url}")
+    private String datasourceUrl;
+
     public static void main(String[] args) {
         SpringApplication.run(Sample1appApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return args -> {
+            System.out.println("Datasource URL: " + datasourceUrl);
+        };
     }
 }
